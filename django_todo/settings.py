49372d038bd28bd5e3210d4b63c74c70cde53483
BOM-98 <>
2023-08-27 12:52:31 +0000

    update page titles

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0b&j04!or0fwl&^uvd!qjyu%!nr96pac$)9mypo!ccz#b=f&02'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-0b&j04!or0fwl&^uvd!qjyu%!nr96pac$)9mypo!ccz#b=f&02')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bom98-django-todo-app-b6978afd2b90.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
